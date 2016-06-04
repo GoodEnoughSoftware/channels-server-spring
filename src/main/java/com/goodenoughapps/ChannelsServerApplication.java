7d98ada5234994851b6dadbf2f36aa32e8000c24
Aaron Vontell <vontell@mit.edu>
2016-06-03 22:52:40 -0400

    Initial commit

---
+++ src/main/java/com/goodenoughapps/ChannelsServerApplication.java
@@ -0,0 +1,13 @@
+package com.goodenoughapps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannelsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannelsServerApplication.class, args);
+	}
+
+}
